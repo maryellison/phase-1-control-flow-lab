b1961124d5b0113653c769da8f00e1a20c1ceb3f
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-09-26 22:16:16 -0400

    commit tree 54de6872ed095b0e936de87e5dc1117b59a96eb9

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetDistance){
+  if (feetDistance <= 400){
+    return 'This one is on me!'
+  } else if (400 < feetDistance && feetDistance <= 2000){
+    return 'That will be twenty bucks.'
+  } else if (2000 < feetDistance && feetDistance <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else {
+      return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  return cityName === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  switch (tipAmount){
+    case 'generous': 
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
